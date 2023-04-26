d38d4c8baae3fd193a72785e024e62398e50a623
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-04-25 22:19:41 -0500

    change on proyects sections for cards animated whit the info

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,158 +3,278 @@ import React from "react";
 const Work = () => {
   return (
     <section className="text-white py-12 px-2 max-w-[1200px] mx-auto">
+
       <h3 className="uppercase text-sm font-semibold text-gray-400" id="Work Experience">
         Work Experience
-      </h3>
+        </h3>
+
       <h2 className="text-3xl font-bold font-Bebas hover:text-lime-400 cursor-pointer mt-2">
         Actualy Work on Self Projets
       </h2>
-      <section className="grid gap-12 mt-12  sm:grid-cols-1">
-        <article class="grid sm:grid-cols-7 text-[22px] sm:text-[16px] py-4 gap-4 font-Bebas text-lime-400">
-          <div class="p-4  hover:bg-[#FFF3E4] hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700 text-[22px]  font-bold">
-              Proyect 1
-            </h3>
-            <p class="mt-2 ">
-              on this proyect im working whit a team from  Academlo, we  done this
-              Store whit, cart darkmode, slider bar, memori main, support window
-              etc. we use CSS and JavaScript.
-              <br /><br />
-            </p>
-            <img className="" src="/images/Academlo_Store.jpg" alt="" />
-            <a
-              href="https://store-academlo-shop.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-4 items  text-gray-400 sm:text-[20px] text-[25px]  hover:text-blue-700 "
-            >
-              click on the project 
-            </a>
-          </div>
-          <div class="p-4 hover:bg-[#FFF3E4]  hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700 text-[22px] font-bold">
-              Proyect 2
-            </h3>
-            <p class="mt-2">
-              Api climate: We use TaildwindCSS, JavaScript, and HTML in React
-              for the finished API. We add modes such as the dark mode that
-              automatically adapts according to the time, a search engine for
-              different places
-            </p>
-            <img className="" src="/images/climate.jpg" alt="" />
-            <a
-              href="https://climate-api.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-2 text-gray-400 hover:text-blue-700 sm:text-[20px] text-[25px]"
-            >
-              click on the project 
-            </a>
-          </div>
-          <div class="p-4 hover:bg-[#FFF3E4]  hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700 text-[22px] font-bold">
-              Proyect 3
-            </h3>
-            <p class="mt-2">
-              in this shop we test our individual skills, using CSS, JavaScript
-              and HTML5. What are you waiting for to visit now
-              GamingSoftwarePereira. this shop is a practice design <br />
-              <br />
-            </p>
 
-            <img className="" src="/images/store.jpg" alt="" />
-            <a
-              href="https://tienda-virtual-1.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-2 text-gray-400 hover:text-blue-700 sm:text-[20px] text-[25px]"
-            >
-              click on the project 
-            </a>
-          </div>
-          <div class="p-4 hover:bg-[#FFF3E4]  hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700  text-[22px] font-bold">
-              Proyect 4
-            </h3>
-            <p class="mt-2">
-              welcome to the galaxy You will make this trip through a space
-              rocket, thus managing to travel to different places and thus
-              discover great things.
-              <br />
-              <br />
-              <br />
-              <br />
-            </p>
-            <img className="" src="/images/galaxy.jpg" alt="" />
-            <a
-              href="https://into-spaceship.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-2 text-gray-400 hover:text-blue-700 sm:text-[20px] text-[25px]"
-            >
-              click on the project 
-            </a>
-          </div>
-          <div class="p-4 hover:bg-[#FFF3E4]  hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700 text-[22px] font-bold">
-              Proyect 5
-            </h3>
-            <p class="mt-2">
-              Welcome join us in this crazy story. We are going on a journey
-              through different dimensions, with our two crazy friends. You
-              cannot imagine the characters that we will find in these different
-              places.
-              <br />
-            </p>
-            <img className="" src="/images/rick_and_morty.jpg" alt="" />
-            <a
-              href="https://rick-and-morty-world.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-2 text-gray-400 hover:text-blue-700 sm:text-[20px] text-[25px]"
-            >
-              click on the project 
-            </a>
-          </div>
-          <div class="p-4 hover:bg-[#FFF3E4]  hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700 text-[22px] font-bold">
-              Proyect 6
-            </h3>
-            <p class="mt-2">
-              In this project we work on the creation of a database,
-               with functions such as user registration, user deletion and data updating.
-               <br /><br /><br /><br />
-            </p>
-            <img className="" src="/images/crud.png" alt="" />
-            <a
-              href="https://crud-data-base.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-2 text-gray-400 hover:text-blue-700 sm:text-[20px] text-[25px]"
-            >
-              click on the project 
-            </a>
-          </div>
-          <div class="p-4 hover:bg-[#FFF3E4]  hover:text-black hover:shadow-lg">
-            <h3 class="text-lg text-gray-700 text-[22px] font-bold">
-              Proyect 7
-            </h3>
-            <p class="mt-2">
-              hi, in this fronted mentor challenge we replicate a stat card. we
-              use TaildwindCSS, HTML 5 and JavaScript. All rights to fronted
-              mentor for being creators of the challenge.
-              <br />
-              <br />
-            </p>
-            <img className="" src="/images/Card_stats.jpg" alt="" />
-            <a
-              href="https://taildwind-vite-and-react.netlify.app/"
-              target="_blank" rel="noopener noreferrer"
-              class="mt-2 text-gray-400 hover:text-blue-700 sm:text-[20px] text-[25px]"
-            >
-              click on the project 
-            </a>
-          </div>
+      
+        <article class="grid grid-cols-1 sm:grid-cols-4 gap-x-[40px] gap-12 text-[22px] sm:text-[16px] font-Bebas "> 
+         
+        
+          <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4  [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/Academlo_Store.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-xl bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl  py-4 ">Academlo Store</h1>
+                  <p class="text-xl text-lime-400">proyect 1</p>
+                  <p className=" text-4xl sm:text-base py-4">
+                    On this proyect im working whit a team from Academlo, we
+                    done this store whit, cart darkmode, slider bar, memori
+                    main, support window etc. we use CSS and JavaScript.
+                  </p>
+                  <a
+                    href="https://store-academlo-shop.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700"
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+            
+            <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4 [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/climate.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-xl bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl  py-4 "> Api climate</h1>
+                  <p class="text-xl text-lime-400">proyect 2</p>
+                  <p className=" text-4xl sm:text-base py-4">
+                  We use TaildwindCSS, JavaScript, and HTML in React
+                  for the finished API. We add modes such as the dark mode that
+                  automatically adapts according to the time, a search engine for
+                  different places.
+                  </p>
+                  <a
+                    href="https://store-academlo-shop.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700"
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4 [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/store.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-xl bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl  py-4 ">Academlo Store</h1>
+                  <p class="text-xl text-lime-400">Proyect 3</p>
+                  <p className=" text-4xl sm:text-base py-4">
+                  in this shop we test our individual skills, using CSS, JavaScript
+                  and HTML5. What are you waiting for to visit now
+                  GamingSoftwarePereira. this shop is a practice design
+                  </p>
+                  <a
+                    href="https://tienda-virtual-1.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700"
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4 [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/galaxy.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-xl bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl  py-4 ">About Galaxys</h1>
+                  <p class="text-xl text-lime-400">proyect 4</p>
+                  <p className=" text-4xl sm:text-base py-4">
+                    welcome to the galaxy You will make this trip through a space
+                    rocket, thus managing to travel to different places and thus
+                    discover great things.
+                  </p>
+                  <a
+                    href="https://into-spaceship.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700"
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4 [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/rick_and_morty.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-xl bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl sm:text-[30px] py-4 "> Rick And Morty</h1>
+                  <p class="text-xl text-lime-400">proyect 5</p>
+                  <p className="text-4xl sm:text-base py-4">
+                  Welcome join us in this crazy story. We are going on a journey
+                  through different dimensions, with our two crazy friends. You
+                  cannot imagine the characters that we will find in these different
+                  places.
+                  </p>
+                  <a
+                    href="https://rick-and-morty-world.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700"
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4 [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/crud.png"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-xl bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl py-4 ">Data Base</h1>
+                  <p class="text-xl text-lime-400">proyect 6</p>
+                  <p className=" text-4xl sm:text-base py-4">
+                  In this project we work on the creation of a database,
+                  with user registration, user deletion and data update functions.
+                  </p>
+                  <a
+                    href="https://crud-data-base.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700 "
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="group h-[500px] w-[600px] sm:h-[400px] sm:w-[300px] py-4 p-4 gap-4 [perspective:1000px]">
+              <div
+                className="relative object-cover h-full w-full mx-auto rounded-[2px] shadow-xl translation-all duration-500
+               [transform-style:preserve-3d] group-hover:[transform:rotatey(180deg)]  border-white border-[4px]"
+              >
+                <div className="absolute  inset-0">
+                  <img
+                    className="h-full w-full  rounded-[1px] object-cover shadow-xl  text-center
+                   text-slate-200 "
+                    src="/images/Card_stats.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="absolute inset-0  h-full rounded-[2px] bg-black/80 px-12
+                 text-center bg-[#FFF3E4]  [transform:rotatey(180deg)] [backface-visibility:hidden]"
+                >
+                  <h1 className="text-3xl py-4  ">Academlo Store</h1>
+                  <p class="text-xl sm:text-[20px] text-lime-400">proyect 7</p>
+                  <p className=" text-4xl sm:text-base py-4">
+                  hi, in this fronted mentor challenge we replicate a stat card. we
+                  use TaildwindCSS, HTML 5 and JavaScript. All rights to fronted
+                  mentor for being creators of the challenge.
+                  </p>
+                  <a
+                    href="https://taildwind-vite-and-react.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="mt-4 items  text-gray-400 sm:text-[23px] text-[25px]  hover:text-blue-700 "
+                  >
+                    click on the project
+                  </a>
+                </div>
+              </div>
+            </div>
+          
         </article>
         <article>
           <img src="" alt="" />
         </article>
       </section>
-    </section>
+    
   );
 };
 
