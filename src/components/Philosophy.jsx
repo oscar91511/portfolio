3b1e870536df17a22389e01c54e74d65839219d6
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-19 20:19:38 -0500

    add curriculum ore cv on contact me

--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -6,16 +6,17 @@ const Philosophy = () => {
       <article className="bg-[#FFF3E4] text-black py-10 px-2 grid gap-12 sm:grid sm:content-between">
         <div>
           <h3 className="text-2xl animate-pulse font-bold mb-4" id="About me">
-            About me & values
+            About me
           </h3>
           <p className="text-gray-500">
-            Hello! I'm <span className="text-blue-500"> Oscar Lopez</span>, a passionate and collaborative front-end
-            developer. I love working in a team and I believe in the importance
-            of open and effective communication. My analytical and detailed
-            approach allows me to solve problems efficiently. I am always
-            updated with the latest trends and I am a constant learner. Excited
-            to take part in challenging projects and build amazing web
-            experiences together!
+            Hello! I'm{" "}
+            <span className="text-blue-500 uppercase"> Oscar Lopez</span>, a
+            passionate and collaborative front-end developer. I love working in
+            a team and I believe in the importance of open and effective
+            communication. My analytical and detailed approach allows me to
+            solve problems efficiently. I am always updated with the latest
+            trends and I am a constant learner. Excited to take part in
+            challenging projects and build amazing web experiences together!
           </p>
         </div>
         <div className="flex flex-col items-center justify-center">
@@ -52,6 +53,14 @@ const Philosophy = () => {
               className="mt-4 font-bold text-black sm:text-[20px] text-[40px] flex items-center"
             >
               <img className="w-[100px]" src="/images/gmail.png" alt="Gmail" />
+            </a>
+            <a
+              href="https://docs.google.com/document/d/1oTekYZ-bR3iDIKBEzIte-Fyh-IZyISOF/edit"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-4 font-bold text-black sm:text-[20px] text-[200px] flex items-center"
+            >
+              <img className="w-[50px]" src="/images/cv.png" alt="cv" />
             </a>
           </div>
         </div>
