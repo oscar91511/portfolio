b1bf6452993b4e8f5ca401a7bc6d54afdab8a027
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-21 12:11:37 -0500

    change on navbar efect, on version mobil navbar are fixed and and on absolute are relative

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,24 +5,20 @@ const Navbar = () => {
   const [isShowNav, setIsShowNav] = useState(false);
   const [currentLanguage, setCurrentLanguage] = useState("en");
   const { t, i18n } = useTranslation();
-  
 
   const handleShowNav = () => {
     setIsShowNav(!isShowNav);
   };
 
   const handleLanguageChange = () => {
-    const newLanguage = currentLanguage === "en" ? "es" : "en"; // We alternate between "is" and "in" each time it is clicked 
-    // Alternamos entre "es" y "en" cada vez que se haga clic
+    const newLanguage = currentLanguage === "en" ? "es" : "en";
     setCurrentLanguage(newLanguage);
-    i18n.changeLanguage(newLanguage); // we change language using hook useTranslation
-     // Cambiamos el idioma usando el hook useTranslation
+    i18n.changeLanguage(newLanguage);
   };
 
-
   return (
-    <header className="flex justify-between py-4  px-2 text-3xl text-white items-center md:px-4 md:py-0 fixed  top-0 left-0 w-full z-30">
-      <h1 className="font-bold  hover:text-lime-400 tracking-wider  animate-pulse font-Robo">
+    <header className="md:relative md:container md:mx-auto flex justify-between py-4 px-2 text-3xl text-white items-center md:px-4 md:py-0 fixed top-0 left-0 w-full z-30">
+      <h1 className="font-bold hover:text-lime-400 tracking-wider animate-pulse font-Robo">
         Oscar L.R
       </h1>
 
@@ -31,37 +27,45 @@ const Navbar = () => {
         className="bx bx-menu-alt-right text-gray-300 text-3xl cursor-pointer md:hidden"
       ></i>
 
+      {/* Fondo oscuro translúcido */}
+      {isShowNav && (
+        <div
+          className="fixed top-0 left-0 w-full h-screen bg-black opacity-50 z-10"
+          onClick={handleShowNav} // Cerrar el menú cuando se haga clic en el fondo oscuro
+        />
+      )}
+
       <nav
-        className={`text-white  absolute  top-full ${
-          isShowNav ? "left-0," : "-left-full"
-        } w-full grid text-lg gap-2  font-Robo font-bold sm:font-Bebas duration-200 md:static md:flex md:w-auto`}
+        className={`text-white absolute top-full ${
+          isShowNav ? "left-0" : "-left-full"
+        } w-full grid text-lg gap-2 font-Robo font-bold sm:font-Bebas duration-200 md:static md:flex md:w-auto z-20`}
       >
+        {/* Contenido de la barra de navegación */}
         <a
-          className="hover:text-lime-400 p-4 duration-200 text-3xl  pl-2 cursor-pointer"
-           onClick={() => handleLanguageChange('es')}
+          className="hover:text-lime-400 p-4 duration-200 text-3xl pl-2 cursor-pointer"
+          onClick={() => handleLanguageChange("es")}
           id="languageButton"
         >
-          {t('languageButton')}
+          {t("languageButton")}
         </a>
         <a
-          className=" hover:text-lime-400 p-4 duration-200 text-3xl pl-2  "
+          className="hover:text-lime-400 p-4 duration-200 text-3xl pl-2"
           href="#Work Experience"
         >
-          {t('workExperience')}
+          {t("workExperience")}
         </a>
         <a
-          className=" hover:text-lime-400 p-4 duration-200 text-3xl  pl-2 "
+          className="hover:text-lime-400 p-4 duration-200 text-3xl pl-2"
           href="#Skillsets"
         >
-           {t('skillsets')}
+          {t("skillsets")}
         </a>
         <a
-          className=" hover:text-lime-400 p-4 duration-200 text-3xl  pl-2 "
+          className="hover:text-lime-400 p-4 duration-200 text-3xl pl-2"
           href="#About me"
         >
-         {t('contactMe')}
+          {t("contactMe")}
         </a>
-        
       </nav>
     </header>
   );
