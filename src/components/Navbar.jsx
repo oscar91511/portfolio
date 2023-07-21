e232bf56e725fee89245aa66e2cbde8adb4854d0
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-21 12:14:05 -0500

    add description on code on english

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,11 +27,11 @@ const Navbar = () => {
         className="bx bx-menu-alt-right text-gray-300 text-3xl cursor-pointer md:hidden"
       ></i>
 
-      {/* Fondo oscuro translúcido */}
+      {/* Translucent dark background / Fondo oscuro translúcido */}
       {isShowNav && (
         <div
           className="fixed top-0 left-0 w-full h-screen bg-black opacity-50 z-10"
-          onClick={handleShowNav} // Cerrar el menú cuando se haga clic en el fondo oscuro
+          onClick={handleShowNav} // Close menu when dark background is clicked / Cerrar el menú cuando se haga clic en el fondo oscuro
         />
       )}
 
@@ -40,7 +40,7 @@ const Navbar = () => {
           isShowNav ? "left-0" : "-left-full"
         } w-full grid text-lg gap-2 font-Robo font-bold sm:font-Bebas duration-200 md:static md:flex md:w-auto z-20`}
       >
-        {/* Contenido de la barra de navegación */}
+        {/* Navigation bar content / Contenido de la barra de navegación */}
         <a
           className="hover:text-lime-400 p-4 duration-200 text-3xl pl-2 cursor-pointer"
           onClick={() => handleLanguageChange("es")}
